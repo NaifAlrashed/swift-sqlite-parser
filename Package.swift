ec2351dfe29fc28feae27af087cb7fa1be3a0c12
Naif <naifdev@gmail.com>
2024-02-09 17:59:10 +0300

    implement select all on users table

--- Package.swift
+++ Package.swift
@@ -5,14 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "swift-sqlite-parser",
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(
             name: "SQLiteParser",
             targets: ["SQLiteParser"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.13.0"),
+    ],
     targets: [
-        .target(name: "SQLiteParser"),
+        .target(
+            name: "SQLiteParser",
+            dependencies: [
+                .product(name: "Parsing", package: "swift-parsing"),
+            ]
+        ),
         .testTarget(
             name: "SQLiteParserTests",
             dependencies: ["SQLiteParser"]
--- Tests/SQLiteParserTests/swift_sqlite_parserTests.swift
+++ Tests/SQLiteParserTests/swift_sqlite_parserTests.swift
@@ -1,13 +1,44 @@
 import XCTest
 @testable import SQLiteParser
+import Parsing
 
 final class SqliteParserTests: XCTestCase {
     func testSelectStatement() throws {
-        let sqliteParser = SQLiteParser()
-        
+        let validSelectStatements = [
+            "SELECT * FROM users;",
+            "SELECT    *     FROM    users;",
+            """
+            SELECT
+                *
+            FROM
+                users;
+            """
+        ]
+        let expectedOutput = SelectStatement(table: "users", columns: .all)
+        try validSelectStatements.forEach { validSelectStatement in
+            let result = try selectParser.parse("SELECT * FROM users;")
+            XCTAssertEqual(result, expectedOutput)
+        }
     }
 }
 
-struct SQLiteParser {
-    
+let selectParser = Parse {
+    "SELECT".utf8
+    Whitespace()
+    "*".utf8
+    Whitespace()
+    "FROM".utf8
+    Whitespace()
+    "users;".utf8
+}.map { _ in
+    SelectStatement(table: "users", columns: .all)
+}
+
+struct SelectStatement: Hashable {
+    let table: String
+    let columns: Columns
+}
+
+enum Columns: Hashable {
+    case all
 }
